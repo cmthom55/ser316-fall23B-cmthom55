99ddbb81b1491c9b3ec5841bd2a5c075ce6ce953
cmthom55 <cmthom55@asu.edu>
2023-11-07 12:24:00 -0800

    Updates Main and Game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //SER316 TASK 2 SPOTBUGS FIX
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
 
-    public static void main(String[] args) {
-        // just some calls
+        //SER316 TASK 2 SPOTBUGS FIX
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
         System.out.println("Getting started");
         Game game = new Game(1);
         System.out.println("Current word: " + game.getAnswer());
@@ -13,24 +19,25 @@ public class Main {
         System.out.println("Automatic guess a");
         System.out.println(game.getProgress());
 
-        // Rough game play
         Game newgame = new Game("Dr. M.", 0);
         System.out.println("Make a guess: ");
         System.out.println(newgame.getProgress());
-        while (newgame.getGameStatus() == 0) {
-            String message = scanner.nextLine();
 
-            if (validateMessage(message)) {
-                System.out.println(newgame.makeGuess(message));
-                System.out.println("Score: " + newgame.getScore());
-                System.out.println(newgame.getProgress());
-            } else {
-                System.out.println("Input is not valid. Please enter only alphabetic characters.");
+        while (newgame.getGameStatus() == 0) {
+            try {
+                String message = bufferedReader.readLine();
+                //SER316 TASK 2 SPOTBUGS FIX
+                if (message != null) {
+                    System.out.println(newgame.makeGuess(message));
+                    System.out.println("Score: " + newgame.getScore());
+                    System.out.println(newgame.getProgress());
+                } else {
+                    System.out.println("Message is null.");
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-    }
-
-    public static boolean validateMessage(String message) {
-        return !message.isEmpty() && message.matches("^[a-zA-Z]*$");
     }
 }
