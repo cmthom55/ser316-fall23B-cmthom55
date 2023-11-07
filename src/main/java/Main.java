f58559e3b0a2fd38a804c22f3ea5fc1dbd91f10b
cmthom55 <cmthom55@asu.edu>
2023-11-07 07:55:43 -0800

    Update Game.java and Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,21 @@ public class Main {
         System.out.println("Getting started");
         Game game = new Game(1);
         System.out.println("Current word: " + game.answer);
-        System.out.println(game.progress);
+        System.out.println(game.getProgress());
         game.makeGuess("a");
         System.out.println("Automatic guess a");
-        System.out.println(game.progress);
+        System.out.println(game.getProgress());
 
 
         // Rough game play
         Game newgame = new Game("Dr. M.", 0 );
         System.out.println("Make a guess: ");
-        System.out.println(newgame.progress);
+        System.out.println(newgame.getProgress());
         while (newgame.getGameStatus() == 0) {
             String message = scanner.nextLine();
             System.out.println(newgame.makeGuess(message));
             System.out.println("Score: " + newgame.score);
-            System.out.println(newgame.progress);
+            System.out.println(newgame.getProgress());
         }
     }
 }
