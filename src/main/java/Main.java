feb2f14a2ac56cd27af40b4f6f3d6becb1e2c044
cmthom55 <cmthom55@asu.edu>
2023-11-07 12:51:30 -0800

    Updates Main and Game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        // just some calls
+    public static void main(String[] args) throws IOException {
+        //SER316 TASK 2 SPOTBUGS FIX
+        InputStreamReader reader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+
+        //SER316 TASK 2 SPOTBUGS FIX
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
         System.out.println("Getting started");
         Game game = new Game(1);
-        System.out.println("Current word: " + game.answer);
-        System.out.println(game.progress);
+        System.out.println("Current word: " + game.getAnswer());
+        System.out.println(game.getProgress());
         game.makeGuess("a");
         System.out.println("Automatic guess a");
-        System.out.println(game.progress);
+        System.out.println(game.getProgress());
 
-
-        // Rough game play
-        Game newgame = new Game("Dr. M.", 0 );
+        Game newgame = new Game("Dr. M.", 0);
         System.out.println("Make a guess: ");
-        System.out.println(newgame.progress);
+        System.out.println(newgame.getProgress());
+
         while (newgame.getGameStatus() == 0) {
-            String message = scanner.nextLine();
-            System.out.println(newgame.makeGuess(message));
-            System.out.println("Score: " + newgame.score);
-            System.out.println(newgame.progress);
+            try {
+                String message = bufferedReader.readLine();
+                //SER316 TASK 2 SPOTBUGS FIX
+                if (message != null) {
+                    System.out.println(newgame.makeGuess(message));
+                    System.out.println("Score: " + newgame.getScore());
+                    System.out.println(newgame.getProgress());
+                } else {
+                    System.out.println("Message is null.");
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
