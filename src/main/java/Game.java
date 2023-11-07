b2eec8ccfcf6ff583763e7c405cdc875b9bd64fc
cmthom55 <cmthom55@asu.edu>
2023-11-07 10:00:51 -0800

    Update Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -43,7 +43,7 @@ public class Game {
      * @return char[] Character Array of the progress.
      */
     public char[] getProgress() {
-        return new this.progress;
+        return this.progress.clone();
     }
 
     /**
